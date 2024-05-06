f86eb55518eb97c9f87da1226980ab5880d170de
Chu Trung Anh <Anh.CT225564@sis.hust.edu.vn>
2024-05-07 00:33:32 +0700

    Add EnvelopedVirus UI

---
+++ sourcecode/src/GUI/EnvelopedVirusController.java
@@ -0,0 +1,23 @@
+package GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class EnvelopedVirusController {
+
+    @FXML
+    void CoronaClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void HIVClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void RotaClicked(MouseEvent event) {
+
+    }
+
+}
--- sourcecode/src/GUI/HomeController.java
+++ sourcecode/src/GUI/HomeController.java
@@ -1,14 +1,34 @@
 package GUI;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class HomeController {
 
     @FXML
     void EnvelopedVirusClicked(MouseEvent event) {
         System.out.println("Enveloped Virus Clicked");
+
+        
+        //Change the scene to EnvelopedVirus.fxml
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("EnvelopedVirus.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
     @FXML
     void NonEnvelopedVirusClicked(MouseEvent event) {
