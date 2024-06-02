94f879a2363ba59f9e4cbdaeb4880552d56b93a3
bachthetrollface <bachthetrollface@gmail.com>
2024-06-02 19:01:56 +0700

    Updated HelpController to inherit from BaseController

--- sourcecode/src/AllJavaCode/GUI/HelpController.java
+++ sourcecode/src/AllJavaCode/GUI/HelpController.java
@@ -1,49 +1,26 @@
 package AllJavaCode.gui;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
 
-public class HelpController implements Initializable {
+public class HelpController extends BaseController {
 
-    private DataModel model;
     @FXML TextArea taDetails;
 
     public HelpController() {
-        this.model = new DataModel();
+        super();
     }
 
     public HelpController(DataModel model) {
-        this.model = model;
+        super(model);
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         taDetails.setEditable(false);
-    }
-
-    @FXML
-    void homeButtonClicked(MouseEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("HomeScene.fxml"));
-            loader.setController(new HomeController(model));
-
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(loader.load()));
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
     
 }
