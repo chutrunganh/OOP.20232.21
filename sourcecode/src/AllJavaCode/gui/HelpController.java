268fa445a48d54a7fda5653c378f668b58f204f6
bachthetrollface <bachthetrollface@gmail.com>
2024-06-03 20:32:50 +0700

    Fixed gui package's naming issue

---
+++ sourcecode/src/AllJavaCode/gui/HelpController.java
@@ -0,0 +1,19 @@
+package AllJavaCode.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelpController extends BaseController {
+
+    public HelpController() {
+        super();
+    }
+
+    public HelpController(DataModel model) {
+        super(model);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+    
+}
